27848c84f56ff735bd5d5a6adf3bb1dfefacbd08
susanna <susanna.dilauro98@gmail.com>
2022-12-09 17:03:32 +0100

    lezione finale

---
+++ D5.js
@@ -0,0 +1,137 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+    Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
+*/
+const pets = ['dog', 'cat', 'hamster', 'redfish']
+
+System.out.print ("[dog, cat, hamster, redfish]")
+
+/* ESERCIZIO 2
+    Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
+*/
+
+const pets = ['Dog', 'Cat', 'Hmaster', 'Redfish']
+
+document.write (pets.sort())
+  
+
+/* ESERCIZIO 3
+    Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
+*/
+const pets = ['Dog', 'Cat', 'Hmaster', 'Redfish']
+
+document.write (pets.reverse())
+
+/* ESERCIZIO 4
+    Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
+*/
+
+const pets = ['Dog', 'Cat', 'Hmaster', 'Redfish']
+const last = arr[arr.length-1];
+console.log(last);
+
+
+/* ESERCIZIO 5
+    Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
+*/
+const cars = [
+  {
+    brand: 'Ford',
+    model: 'Fiesta',
+    color: 'red',
+    licensePlate: 'ED485KC'
+    trims: ['titanium', 'st', 'active'],
+  },
+  {
+    brand: 'Peugeot',
+    model: '208',
+    color: 'blue',
+    licensePlate: 'DR479KW'
+    trims: ['allure', 'GT'],
+  },
+  {
+    brand: 'Volkswagen',
+    model: 'Polo',
+    color: 'black',
+    licensePlate: 'GA789RW'
+    trims: ['life', 'style', 'r-line'],
+  },
+]
+car.licensePlate = 'ED485KC';
+console.log(car.licensePlate);
+console.log(car);
+
+car.licensePlate = 'DR479KW';
+console.log(car.licensePlate);
+console.log(car);
+
+car.licensePlate = 'GA789RW';
+console.log(car.licensePlate);
+console.log(car);
+
+
+/* ESERCIZIO 6
+    Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
+    Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
+*/
+
+delete car.propertyToRemove;
+console.log(car);
+
+car.pop(Trims);
+
+
+/* ESERCIZIO 7
+    Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
+*/
+const justTrims = []
+for (let i = 0; i < cars.length; i++){
+  justTrims.push (cars[i].trims[0])
+}
+console.log(justTrims)
+
+/* ESERCIZIO 8
+    Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
+    "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
+*/
+
+/* ESERCIZIO 9
+    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
+*/
+const numericArray = [
+  6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+]
+while(numero <=12)
+
+/* ESERCIZIO 10
+    Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
+    dell'alfabeto italiano.
+    es. [f, b, e] --> [6, 2, 5]
+*/
+const charactersArray = ["g", "n", "u", "z", "d"]
+
+switch (charactersArray) {
+  case "g":
+     ( 'diventa 7' );
+    break;
+  case "n":
+    ( 'diventa 12' );
+    break;
+  case "u":
+    ( 'diventa 19' );
+    break;
+    case "z":
+      ('diventa 21');
+    case "d":
+        ('diveta 4');
+  default:
+    console.log('alfabetoitaliano');
+}
+
